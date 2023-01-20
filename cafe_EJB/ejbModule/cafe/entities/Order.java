de9eb6bafb6a6de054b5287f7e478de4818641c1
Kvmlg <101001317+Kvmlg@users.noreply.github.com>
2023-01-20 21:16:33 +0100

    final

---
+++ cafe_EJB/ejbModule/cafe/entities/Order.java
@@ -0,0 +1,93 @@
+package cafe.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * The persistent class for the orders database table.
+ * 
+ */
+@Entity
+@Table(name="orders")
+@NamedQuery(name="Order.findAll", query="SELECT o FROM Order o")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private double amount;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateOrder;
+
+	//bi-directional many-to-one association to Orderproduct
+	@OneToMany(mappedBy="order")
+	private List<Orderproduct> orderproducts;
+
+	//bi-directional many-to-one association to User
+	@ManyToOne
+	private User user;
+
+	public Order() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getDateOrder() {
+		return this.dateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		this.dateOrder = dateOrder;
+	}
+
+	public List<Orderproduct> getOrderproducts() {
+		return this.orderproducts;
+	}
+
+	public void setOrderproducts(List<Orderproduct> orderproducts) {
+		this.orderproducts = orderproducts;
+	}
+
+	public Orderproduct addOrderproduct(Orderproduct orderproduct) {
+		getOrderproducts().add(orderproduct);
+		orderproduct.setOrder(this);
+
+		return orderproduct;
+	}
+
+	public Orderproduct removeOrderproduct(Orderproduct orderproduct) {
+		getOrderproducts().remove(orderproduct);
+		orderproduct.setOrder(null);
+
+		return orderproduct;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
